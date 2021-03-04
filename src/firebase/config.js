834c7348c7f406260a519f7396ca5ab931f329f1
ChinYikMing <58044437+ChinYikMing@users.noreply.github.com>
2021-03-05 01:51:13 +0800

    Update config.js

--- src/firebase/config.js
+++ src/firebase/config.js
@@ -2,15 +2,15 @@ import firebase from 'firebase/app'
 import 'firebase/auth'
 import 'firebase/firestore'
 
-const firebaseConfig = {
-  apiKey: "AIzaSyDNQ980wAi6aRcJpUemrVkYt2HA8ds4E9c",
-  authDomain: "clone-84f71.firebaseapp.com",
-  databaseURL: "https://clone-84f71.firebaseio.com",
-  projectId: "clone-84f71",
-  storageBucket: "clone-84f71.appspot.com",
-  messagingSenderId: "315056278744",
-  appId: "1:315056278744:web:a3f51dffc1b3122ff75bad",
-  measurementId: "G-8H9NVZXYTR"
+var firebaseConfig = {
+  apiKey: "API_KEY",
+  authDomain: "PROJECT_ID.firebaseapp.com",
+  databaseURL: "https://PROJECT_ID.firebaseio.com",
+  projectId: "PROJECT_ID",
+  storageBucket: "PROJECT_ID.appspot.com",
+  messagingSenderId: "SENDER_ID",
+  appId: "APP_ID",
+  measurementId: "G-MEASUREMENT_ID",
 };
 
 firebase.initializeApp(firebaseConfig);
